a26e5990cbf24ba899db9592734e36e22a9fa6e8
Munazzah Hassan Khan <munazzah.hassan@gmail.com>
2021-05-25 14:08:29 -0700

    finished lab excercise

--- hackbright.py
+++ hackbright.py
@@ -42,22 +42,59 @@ def make_new_student(first_name, last_name, github):
     Given a first name, last name, and GitHub account, add student to the
     database and print a confirmation message.
     """
-    pass
+    QUERY = """
+        INSERT INTO students (first_name,last_name,github)
+        VALUES(:first_name,:last_name,:github)
+    """
+    db.session.execute(QUERY, {'first_name': first_name, 
+                               'last_name': last_name, 
+                               'github': github})
+    
+    db.session.commit()
+    print(f"Successfully added student: {first_name} {last_name}")
 
 
 def get_project_by_title(title):
     """Given a project title, print information about the project."""
-    pass
+    QUERY = """
+        SELECT id, title, description, max_grade
+        FROM projects
+        WHERE title = :title
+        """
+    db_cursor = db.session.execute(QUERY,{'title': title})
+
+    row = db_cursor.fetchone()
+
+    print(f'ID: {row[0]}\nPROJECT TITLE: {row[1]}\nDESCRIPTION: {row[2]}\nMAX GRADE: {row[3]}')
 
 
 def get_grade_by_github_title(github, title):
     """Print grade student received for a project."""
-    pass
+    QUERY = """
+            SELECT grade
+            FROM grades
+            WHERE student_github = :github AND project_title = :title
+            """
+    db_cursor = db.session.execute(QUERY, {'github': github, 
+                                           'title': title})
+    row = db_cursor.fetchone()
+    
+    print(f"The student received {row[0]} on the project {title}")
 
 
 def assign_grade(github, title, grade):
     """Assign a student a grade on an assignment and print a confirmation."""
-    pass
+    QUERY = """
+        INSERT INTO grades (student_github,project_title,grade)
+        VALUES (:github, :title, :grade)
+        """
+    db.session.execute(QUERY, {'github':github,
+                                'title' :title,
+                                'grade':grade})
+    db.session.commit()
+
+    print(f'{github} was given a {grade} on {title}.')
+
 
 
 def handle_input():
